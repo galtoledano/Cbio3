d5b115c288c728aeb30c7dd0c9360e001704bba5
galtoledano <>
2020-01-07 13:06:12 +0200

    start to write the algorithm

--- ZOOPS_EM.py
+++ ZOOPS_EM.py
@@ -1,5 +1,7 @@
 import argparse
 from motif_find import *
+from itertools import groupby
+import numpy as np
 
 
 def parse_args():
@@ -13,8 +15,66 @@ def parse_args():
     return parser.parse_args()
 
 
+def init_q(seq_lst, seed):
+    counter = 0
+    for seq in seq_lst:
+        if seed in seq:
+            counter += 1
+    return counter / len(seq_lst)
+
+
+def fastaread(fasta_name):
+    f = open(fasta_name)
+    faiter = (x[1] for x in groupby(f, lambda line: line.startswith(">")))
+    for header in faiter:
+        header = next(header)[1:].strip()
+        seq = "".join(s.strip() for s in next(faiter))
+        yield header, seq
+
+
+def read_fasta(fasta_name):
+    seq_lst = []
+    iter = fastaread(fasta_name)
+    fasta_iter = iter.__next__()[1]
+    while fasta_iter:
+        try:
+            seq_lst.append(fasta_iter)
+            fasta_iter = iter.__next__()[1]
+        except StopIteration:
+            return seq_lst
+    return seq_lst
+
+
+def init_emission(seed, alpha):
+    emission = np.full((4, len(seed)), alpha)
+    for i in range(len(seed)):
+        emission[converting_dict[seed[i]]][i] = 1 - (3 * alpha)
+    return emission
+
+
+def edit_emission(mat):
+    """
+    This function parses the given tsv file of the emission matrix, to a numpy matrix
+    :param mat: the emission matrix
+    :return: the the emission matrix
+    """
+    B_1_2 = np.full(K_ADDITION, U_PROPABILITY)
+    mat = np.vstack([mat, B_1_2])
+    B_end_B_start = np.zeros(K_ADDITION)
+    mat = np.vstack([mat, B_end_B_start])
+    mat = np.vstack([B_1_2, mat])
+    mat = np.vstack([B_end_B_start, mat])
+    k = mat.shape[0]
+    hat_vector = np.zeros((1, k))
+    dollar_vector = np.zeros((1, k))
+    hat_vector[0][0], dollar_vector[0][k - 1] = 1, 1
+    mat = np.insert(mat, 4, hat_vector, axis=1)
+    mat = np.insert(mat, 5, dollar_vector, axis=1)
+    return log_marix(mat), k
+
 def main():
     args = parse_args()
+
 
     # Initialize the model:
     #   Transitions:
@@ -33,6 +93,28 @@ def main():
     #   - motif_profile.txt
     #   - motif_positions.txt
 
+
+def write_profile(emission, p, q):
+    motif_profile = open("motif_profile.txt", "w")
+    for line in emission:
+        for char in line:
+            motif_profile.write(str(round(char, 2)) + "\t")
+        motif_profile.write("\n")
+    motif_profile.write(str(round(q, 2)) + "\n")  # p
+    motif_profile.write(str(round(p, 2)) + " \n")
+    motif_profile.close()
+
+
 if __name__ == "__main__":
-    main()
+    # main()
+    seed = "AGGC"
+    seq_array = read_fasta("gal<3.fasta")
+    q = init_q(seq_array, seed)
+    p = 0.1
+    emission = init_emission(seed, 0.1)
+    mat, k = edit_emission(emission)
+    tau = init_tau(k, p, q)
+    for i in range(len(seq_array)):
+        seq_array[i] = edit_sequence(seq_array[i])
+    write_profile(emission, p, q)
 
--- motif_find.py
+++ motif_find.py
@@ -1,23 +1,47 @@
+#############################################################
+# CBIO EX2 - ZOOPS Model Motif Finding
+# galtoledano, 204631295
+# omriyavne, 316520097
+#############################################################
+
 import argparse
 import numpy as np
+from scipy.special import logsumexp
 
+U_PROPABILITY = 0.25
+
+"A dictionary containing their base values ​​and numeric values"
 converting_dict = {"A": 0, "C": 1, "G": 2, "T": 3, "^": 4, "$": 5}
 
+"default line length for printing"
+LINE_LENGTH = 50
+K_ADDITION = 4
 
-# def letters_to_numbers(seq, d):
-#     """
-#     converting by dictionary dna to numbers and  numbers to dna
-#     :param seq: the first alignment
-#     :param d: the dictionary
-#     :return: the converted seq
-#     """
-#     keys, choices = list(zip(*d.items()))
-#     seq_a = np.array(keys)[:, None, None] == seq
-#     seq = np.select(seq_a, choices)[0]
-#     return seq
+BACKGROUND = "B"
+MOTIF = "M"
+STARTING_CHAR_SEQ = "^"
+ENDING_CHAR_SEQ = "$"
+
+
+def log_marix(matrix):
+    """
+    This function performs a log operation on a matrix
+    :param matrix: the given matrix
+    :return: the matrix after the log operation
+    """
+    with np.errstate(divide='ignore'):
+        matrix = np.log(matrix)
+        return matrix
 
 
 def init_tau(k, p, q):  # whereas k = k + 4
+    """
+    This function inits the tau matrix
+    :param k: the number of states
+    :param p: p value
+    :param q: q value
+    :return: tau matrix
+    """
     tau_matrix = np.zeros((k, k))
     for row in range(2, k - 2):
         tau_matrix[row][row + 1] = 1
@@ -27,18 +51,28 @@ def init_tau(k, p, q):  # whereas k = k + 4
     tau_matrix[1][2] = p
     tau_matrix[k - 2][k - 2] = 1 - p
     tau_matrix[k - 2][k - 1] = p
-    return tau_matrix
+    return log_marix(tau_matrix)
 
 
 def edit_sequence(seq):
+    """
+    This matrix adds the starting and the ending to chars to a given sequences
+    :param seq: the edgiven sequence
+    :return: the edited sequence
+    """
     return "^" + seq + "$"
 
 
 def read_emission(file):
+    """
+    This function parses the given tsv file of the emission matrix, to a numpy matrix
+    :param file: the tsv file
+    :return: the the emission matrix
+    """
     mat = np.genfromtxt(file, delimiter='\t', skip_header=1)
-    B_1_2 = np.full(4, 0.25)
+    B_1_2 = np.full(K_ADDITION, U_PROPABILITY)
     mat = np.vstack([mat, B_1_2])
-    B_end_B_start = np.zeros(4)
+    B_end_B_start = np.zeros(K_ADDITION)
     mat = np.vstack([mat, B_end_B_start])
     mat = np.vstack([B_1_2, mat])
     mat = np.vstack([B_end_B_start, mat])
@@ -48,30 +82,144 @@ def read_emission(file):
     hat_vector[0][0], dollar_vector[0][k - 1] = 1, 1
     mat = np.insert(mat, 4, hat_vector, axis=1)
     mat = np.insert(mat, 5, dollar_vector, axis=1)
-    return mat, k
+    return log_marix(mat), k
 
 
-def viterbi(seq, tau, emisson):
+def viterbi(seq, tau, emission_table):
+    """
+    This function performs the viterbi algorithm
+    :param seq:  the given sequence
+    :param tau: the tau matrix
+    :param emission_table: the emission table
+    :return: the v matrix, and the t matrix(for the traceback)
+    """
     k = emission_table.shape[0]
     v_matrix = np.zeros((k, len(seq)))
     t_matrix = np.zeros((k, len(seq)))
     v_matrix[0][0], t_matrix[0][0] = 1, 1
+    v_matrix = log_marix(v_matrix)
     for letter in range(1, len(seq)):
-        for state in range(k):
-            prev_col = v_matrix[:, letter-1]
-            # tau_row = tau[state]
-            tau_col = tau[:, state]
-            temp_mult = prev_col * tau_col
-            max_val = max(temp_mult)
-            argmax_index = np.where(temp_mult == max_val)[0][0]
-            col_index = converting_dict[seq[letter]]
-            v_matrix[state][letter] = max_val * emisson[state][col_index]
-            t_matrix[state][letter] = argmax_index
+        sum_of_cols = v_matrix[:, letter - 1].reshape(-1, 1) + tau
+        max_val = np.max(sum_of_cols, axis=0).T
+        argmax_index = np.argmax(sum_of_cols, axis=0).T
+        v_matrix[:, letter] = max_val + emission_table[:, converting_dict[seq[letter]]]
+        t_matrix[:, letter] = argmax_index
     return v_matrix, t_matrix
 
 
+def trace_viterbi(v_matrix, t_matrix):
+    """
+    This function traces the viterbi matrix, in order to create the viterbi sequence
+    :param v_matrix: the viterbi matrix
+    :param t_matrix: the trace matrix of indices
+    :return: the viterbi sequences, which contains either B chars(background), or M chars(motif)
+    """
+    viterbi_seq = ""
+    last_col = v_matrix[:, len(v_matrix[0]) - 1]
+    curr = np.where(last_col == max(last_col))[0][0]
+    length = len(v_matrix) - 1
+    for letter in range(len(v_matrix[0]) - 1, 1, -1):  # todo
+        curr = t_matrix[int(curr)][letter]
+        if curr == 0 or curr == 1 or curr == length or curr == length - 1:
+            viterbi_seq = BACKGROUND + viterbi_seq
+        else:
+            viterbi_seq = MOTIF + viterbi_seq
+    return viterbi_seq
+
+
+def forward_algorithm(seq, tau_mat, emission_matrix, k):
+    """
+    This function performs the forward algorithm
+    :param seq: the given sequence
+    :param tau_mat: the tau matrix
+    :param emission_matrix:  the emission matrix
+    :param k: the number of states
+    :return: the forward matrix
+    """
+    f_mat = np.zeros((k, len(seq)))
+    f_mat[0][0] = 1
+    f_mat = log_marix(f_mat)
+    for letter in range(1, len(seq)):
+        sum_of_cols = f_mat[:, letter - 1].reshape(-1, 1) + tau_mat
+        f_mat[:, letter] = logsumexp(sum_of_cols, axis=0) + emission_matrix[:, converting_dict[seq[letter]]]
+    return f_mat
+
+
+def backward_algorithm(seq, tau_mat, emission_matrix, k):
+    """
+    This function performs the backward algorithm
+    :param seq: the given sequence
+    :param tau_mat: the tau matrix
+    :param emission_matrix:  the emission matrix
+    :param k: the number of states
+    :return: the backward matrix
+    """
+    b_mat = np.zeros((k, len(seq)))
+    b_mat[-1][-1] = 1
+    b_mat = log_marix(b_mat)
+    for letter in range(len(seq) - 1, 0, -1):
+        sum_of_cols = b_mat[:, letter].reshape(-1, 1) + tau_mat.T + emission_matrix[:,
+                                                                    converting_dict[seq[letter]]].reshape(-1,
+                                                                                                          1)
+        b_mat[:, letter - 1] = logsumexp(sum_of_cols, axis=0)
+    return b_mat
+
+
+def posterior(f_mat, b_mat, k, seq):
+    """
+    This function uses the result of the forward and backward algorithms in order to find a given seq's
+    hidden states
+    :param f_mat: the forward matrix
+    :param b_mat: the backward matrix
+    :param k: the number of states
+    :param seq: the given sequence
+    :return: the posterior matrix
+    """
+    post_seq = ""
+    post_mat = f_mat + b_mat
+    for letter in range(1, len(seq) - 1):
+        letter_col = post_mat[:, letter]
+        curr = np.argmax(letter_col)
+        if curr < 2 or curr >= k - 2:
+            post_seq += BACKGROUND
+        else:
+            post_seq += MOTIF
+    return post_seq
+
+
+def print_seq_outputs(original_seq, viterbi_seq):
+    """
+    This function prints the given sequence, and the viterbi/posterior sequence in the needed format
+    :param original_seq:  the given sequence
+    :param viterbi_seq: the viterbi/posterior sequence
+    """
+    index_org_seq = 0
+    index_viterbi_seq = 0
+    while index_org_seq != len(original_seq):
+        leftover_length = len(original_seq) - index_org_seq
+        if leftover_length <= LINE_LENGTH:
+            for i in range(leftover_length):
+                print(viterbi_seq[index_viterbi_seq], end="")
+                index_viterbi_seq += 1
+            print()
+            for i in range(leftover_length):
+                print(original_seq[index_org_seq], end="")
+                index_org_seq += 1
+        else:
+            for i in range(LINE_LENGTH):
+                print(viterbi_seq[index_viterbi_seq], end="")
+                index_viterbi_seq += 1
+            print()
+            for i in range(LINE_LENGTH):
+                print(original_seq[index_org_seq], end="")
+                index_org_seq += 1
+            print("\n")
+
 
 def main():
+    """
+    The main function that runs the program based on the input args from the cmd
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('--alg', help='Algorithm (e.g. viterbi)', required=True)
     parser.add_argument('seq', help='A sequence over the alphabet [A,C,G,T] (e.g. ACTGGACTACGTCATGCA)')
@@ -80,45 +228,33 @@ def main():
     parser.add_argument('q', help='transition probability q (e.g. 0.5)', type=float)
     args = parser.parse_args()
 
+    emission_table, k = read_emission(args.initial_emission)
+    p = args.p
+    q = args.q
+    tau = init_tau(k, p, q)
+    seq = edit_sequence(args.seq)
+
     if args.alg == 'viterbi':
-        raise NotImplementedError
+        v_matrix, t_matrix = viterbi(seq, tau, emission_table)
+        viterbi_seq = trace_viterbi(v_matrix, t_matrix)
+        print_seq_outputs(args.seq, viterbi_seq)
 
     elif args.alg == 'forward':
-        raise NotImplementedError
+        f_mat = forward_algorithm(seq, tau, emission_table, k)
+        print(f_mat[-1][-1])
 
     elif args.alg == 'backward':
-        raise NotImplementedError
+        b_mat = backward_algorithm(seq, tau, emission_table, k)
+        print(b_mat[0][0])
 
     elif args.alg == 'posterior':
+        f_mat = forward_algorithm(seq, tau, emission_table, k)
+        b_mat = backward_algorithm(seq, tau, emission_table, k)
+        post_seq = posterior(f_mat, b_mat, k, seq)
+        print_seq_outputs(args.seq, post_seq)
+    else:
         raise NotImplementedError
-
-
-def trace_viterbi(v_matrix, t_matrix):
-    viterbi_seq = ""
-    last_col = v_matrix[:, len(v_matrix[0]) - 1]
-    curr = np.where(last_col == max(last_col))[0][0]
-    length = len(v_matrix) - 1
-    for letter in range(len(v_matrix[0]) - 1, 1, -1):  # until 1? 0?
-        curr = t_matrix[int(curr)][letter]
-        if curr == 0 or curr == 1 or curr == length or curr == length - 1:
-            viterbi_seq = "B " + viterbi_seq
-        else:
-            viterbi_seq = "M " + viterbi_seq
-
-    return viterbi_seq
 
 
 if __name__ == '__main__':
-    # main()
-    emission_table, k = read_emission("tata.tsv")
-    # k = emission_table.shape[0]
-    tau = init_tau(k, 0.1, 0.1)
-    # seq = "TCGAATCCGTACGGTATTAAGTACGGCGCCTCGAATTCGAATCCGTACGGCGCCCCCCGTACGGCGCCTCGAAT"
-    # seq = "TAGG"
-    seq = "CTATTAAG"
-    seq = edit_sequence(seq)
-    # converted_seq = letters_to_numbers(seq, converting_dict)
-    v_matrix, t_matrix = viterbi(seq, tau, emission_table)
-    print(trace_viterbi(v_matrix, t_matrix))
-
-
+    main()
