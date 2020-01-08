ede7245fe686520078bf1bb5b7d508e04253f807
galtoledano <>
2020-01-08 13:48:22 +0200

    fixed some bugs

--- ZOOPS_EM.py
+++ ZOOPS_EM.py
@@ -1,7 +1,8 @@
 import argparse
-from motif_find import *
+import motif_find as mf
 from itertools import groupby
 import numpy as np
+from scipy.special import logsumexp
 
 
 def parse_args():
@@ -48,7 +49,7 @@ def read_fasta(fasta_name):
 def init_emission(seed, alpha):
     emission = np.full((4, len(seed)), alpha)
     for i in range(len(seed)):
-        emission[converting_dict[seed[i]]][i] = 1 - (3 * alpha)
+        emission[mf.converting_dict[seed[i]]][i] = 1 - (3 * alpha)
     return emission
 
 
@@ -58,9 +59,9 @@ def edit_emission(mat):
     :param mat: the emission matrix
     :return: the the emission matrix
     """
-    B_1_2 = np.full(K_ADDITION, U_PROPABILITY)
+    B_1_2 = np.full(mf.K_ADDITION, mf.U_PROPABILITY)
     mat = np.vstack([mat, B_1_2])
-    B_end_B_start = np.zeros(K_ADDITION)
+    B_end_B_start = np.zeros(mf.K_ADDITION)
     mat = np.vstack([mat, B_end_B_start])
     mat = np.vstack([B_1_2, mat])
     mat = np.vstack([B_end_B_start, mat])
@@ -70,7 +71,7 @@ def edit_emission(mat):
     hat_vector[0][0], dollar_vector[0][k - 1] = 1, 1
     mat = np.insert(mat, 4, hat_vector, axis=1)
     mat = np.insert(mat, 5, dollar_vector, axis=1)
-    return log_marix(mat), k
+    return mf.log_marix(mat), k
 
 def main():
     args = parse_args()
@@ -94,6 +95,7 @@ def main():
     #   - motif_positions.txt
 
 
+
 def write_profile(emission, p, q):
     motif_profile = open("motif_profile.txt", "w")
     for line in emission:
@@ -105,25 +107,29 @@ def write_profile(emission, p, q):
     motif_profile.close()
 
 
-def em(seq_array, tau, emission, k):
+def em(seq_array, tau, emission, k, seed):
     forward = []
     backward = []
+    N_k_x = np.zeros((k, 6))
+    N_k_x[0][4], N_k_x[k-1][5] = 1, 1
+    N_k_x = mf.log_marix(N_k_x)
+    N_k_l = mf.log_marix(np.zeros((k, k)))
+    defult_emm, _ = edit_emission(init_emission(seed, 0.25))
+
     # while True:
     for g in range(5):
         for seq in seq_array:
-            forward.append(forward_algorithm(seq, tau, emission, k))
-            backward.append(backward_algorithm(seq, tau, emission, k))
+            forward.append(mf.forward_algorithm(seq, tau, emission, k))
+            backward.append(mf.backward_algorithm(seq, tau, emission, k))
         # E sage:
-        N_k_x = np.zeros((k, 6))
-        N_k_l = np.zeros((k, k))
         for seq_index in range(len(seq_array)):
+            seq = seq_array[seq_index]
             for letter_index in range(1, len(seq_array[seq_index]) - 1):  # todo : skip ^ and $ ?
                 # calculate N_k_x
-                seq = seq_array[seq_index]
+                pos_val = forward[seq_index][-1][-1]  # todo : is this the real pos value ?
                 posterior_mat = forward[seq_index] + backward[seq_index]
-                pos_val = posterior_mat[-1][-1]  # todo : is this the real pos value ?
                 vec = posterior_mat[:, letter_index] - pos_val
-                curr_letter = converting_dict[seq[letter_index]]
+                curr_letter = mf.converting_dict[seq[letter_index]]
                 for i in range(len(vec)):
                     N_k_x[:, curr_letter][i] = logsumexp([N_k_x[:, curr_letter][i], vec[i]])
                 # temp = logsumexp([N_k_x[:, curr_letter], vec])
@@ -135,20 +141,32 @@ def em(seq_array, tau, emission, k):
                         f = forward[seq_index][state1][letter_index - 1]
                         b = backward[seq_index][state2][letter_index]
                         t = tau[state1][state2]
-                        e = emission[state2][converting_dict[seq[letter_index]]]
+                        if state2 <= 1 or state2 >= k-1:
+                            e = defult_emm[state2][mf.converting_dict[seq[letter_index]]]
+                        else:
+                            e = emission[state2][mf.converting_dict[seq[letter_index]]]
                         NKL = f + b + t + e - pos_val
                         N_k_l[state1][state2] = logsumexp([N_k_l[state1][state2], NKL])
 
                 # update emission
                 e_sums_vec = np.array(logsumexp(N_k_x, axis=1)).reshape((-1, 1))
                 emission = N_k_x - e_sums_vec
+                v = np.full((len(emission), 1), -np.inf)
+                emission = np.where(np.isnan(emission), v, emission)
 
                 # update tau
-                t_sums_vec = np.array(logsumexp(N_k_l, axis=1)).reshape((-1, 1))
-                tau = N_k_l - t_sums_vec
-        print("YAYYYY")
+
+                # t_sums_vec = np.array(logsumexp(N_k_l, axis=1)).reshape((-1, 1))
+                # tau = N_k_l-t_sums_vec
+                # v = np.full((len(tau), 1), -np.inf)
+                # tau = np.where(np.isnan(tau), v, tau)
+                p = N_k_l[1][2]
+                q = N_k_l[0][1]
+                tau = mf.init_tau(k, p, q)
 
 
+                # print("iter with " + str(letter_index))
+                # print(tau)
 
 
 if __name__ == "__main__":
@@ -160,7 +178,7 @@ if __name__ == "__main__":
     emission = init_emission(seed, 0.1)
     write_profile(emission, p, q)
     mat, k = edit_emission(emission)
-    tau = init_tau(k, p, q)
+    tau = mf.init_tau(k, p, q)
     for i in range(len(seq_array)):
-        seq_array[i] = edit_sequence(seq_array[i])
-    em(seq_array, tau, mat, k)
+        seq_array[i] = mf.edit_sequence(seq_array[i])
+    em(seq_array, tau, mat, k, seed)
